3294bafc5fa39c89216208e00f171502c260422d
tareq <tareqhasan9213@gmail.com>
2021-01-05 17:18:21 +0600

    final-submission

---
+++ accounts/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'index.html')
+        else:
+            messages.info(request, 'invalid credentials')
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'username taken')
+            else:
+                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                                email=email, password=password1)
+                user.save()
+                print('user created')
+                return render(request, 'index.html')
+            return render(request, 'register.html')
+        else:
+            messages.info(request, 'password not matched')
+            return redirect('register')
+
+
+
+    else:
+        return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'index.html')
